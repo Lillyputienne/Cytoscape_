0d884a6820ad6d65385ecbfe3410c6472f444eac
joud27 <131020736+joud27@users.noreply.github.com>
2023-05-16 17:16:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ var p_value = '';
 var input_abundance = document.querySelector('#abundance');
 var abundance = '';
 
+let elk_algorithms = {
+  'elk_disco': 'org.eclipse.elk.disco',
+  'elk_force': 'org.eclipse.elk.force',
+  'elk_spore': 'org.eclipse.elk.sporeOverlap',
+}
+
+let elk_layout = {
+  'edgeRouting': 'organic',
+  'algorithm': '',
+  'crossingMinimization': 'LAYER_INTERCHANGE',
+  'layered.spacing.nodeNodeBetweenLayer': 100,
+  'layered.spacing.nodeNodeWithinLayer': 50,
+  'layered.spacing.edgeNodeBetweenLayer': 50,
+  'spacing.edgeEdge': 50,
+  'spacing.nodeNode': 50,
+  'spacing.edgeNode': 50,
+  'edgeLabelPlacement': 'NONE'
+}
+
+
 input_file.addEventListener('input',function() {
   file = input_file.files[0];
 });
@@ -31,8 +51,12 @@ input_abundance.addEventListener('input', function() {
 
 function min_max_ab(json,abundance) {
   var min_ab = Infinity;
+  let sizes = [];
   for (let node of json.nodes) {
     if (`${abundance}` in node.data) {
+      if(!sizes.includes(node.data[abundance])){
+        sizes.push(node.data[abundance]);
+      }
       if (node.data[abundance] < min_ab) {
         min_ab = node.data[abundance];
       }
@@ -47,14 +71,30 @@ function min_max_ab(json,abundance) {
       }
     }
   }
-  document.getElementById("minValue").textContent = min_ab.toFixed(2);
-  document.getElementById("maxValue").textContent = max_ab.toFixed(2);
 
+  sizes.sort((a, b) => {
+    return a - b;
+  });
+
+  var smaller_half = sizes.slice(0, sizes.length / 2);
+  var bigger_half = sizes.slice(sizes.length / 2 + 1);
+
+  let sum_smaller_half = smaller_half.reduce((a, b) => a + b, 0);
+  let avg_smaller_half = (sum_smaller_half / smaller_half.length) || 0;
+
+  let sum_bigger_half = bigger_half.reduce((a, b) => a + b, 0);
+  let avg_bigger_half = (sum_bigger_half / bigger_half.length) || 0;
+  
+  
+  document.getElementById("sq1").textContent = min_ab.toFixed(2);
+  document.getElementById("sq2").textContent = avg_smaller_half.toFixed(2);
+  document.getElementById("sq3").textContent = avg_bigger_half.toFixed(2);
+  document.getElementById("sq4").textContent = max_ab.toFixed(2);
   return [min_ab, max_ab];
 }
 
 document.getElementById('abundance').addEventListener('change', function() {
-  document.getElementById('node-selction').innerText = this.value;
+  document.getElementById('node-selection').innerText = this.value;
 });
 
 
@@ -64,7 +104,7 @@ function afficher_cytoscape(json, log_ratio, p_value, abundance) {
   console.log(p_value);
   console.log(`node[${abundance}]`);
 
-    var cy = window.cy = cytoscape({
+    var settings = {
       container: document.getElementById('cy'),
       
       boxSelectionEnabled: false,
@@ -148,7 +188,22 @@ function afficher_cytoscape(json, log_ratio, p_value, abundance) {
         numIter: 2500,
         tile: true,
       }
-    });
+    };
+
+    var selected_algorithm = document.getElementById('algorithm').value;
+
+    switch(selected_algorithm){
+      case 'fcose': break; // since this is the layout default value
+      case 'cola': break; //! not implemented yet
+      case 'cise': break; //! not implemented yet
+      case '': break; // in case it is empty
+      default: // elk
+        elk_layout.algorithm = elk_algorithms[selected_algorithm];
+        settings.layout.name = 'elk';
+        settings.layout.elk = elk_layout;
+    }
+    
+    var cy = window.cy = cytoscape(settings);
   
 };
 
