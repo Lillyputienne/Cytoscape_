41d71e513fb48201699aec8efcf6f12e0f72c117
ecoutaud <131028820+ecoutaud@users.noreply.github.com>
2023-05-19 17:49:20 +0200

    Add files via upload

--- HTML/js/donwload.js
+++ HTML/js/donwload.js
@@ -1,7 +1,7 @@
+// Creation of variables for the different parameters given by the user.
 var file_name = '';
 document.querySelector('#json_input_file').addEventListener('input',function() {
   file_name = this.files[0].name.split(".")[0];
-  console.log(file_name);
 });
 
 var log_ratio = '';
@@ -25,45 +25,44 @@ document.querySelector('#algorithm').addEventListener('input',function() {
 })
 
 
-
+// JPEG Download
 document.getElementById('jpeg').addEventListener('click', function () {
-  var donwload_name = `${file_name}_${p_value}_${log_ratio}_${abundance}_${algo}.jpeg`;
+  var download_name = `${file_name}_${p_value}_${log_ratio}_${abundance}_${algo}.jpeg`;
   var elementToDownload = document.getElementById('graph');
-    html2canvas(elementToDownload).then(function(canvas) {
-      var link = document.createElement('a');
-      link.download = donwload_name;
-      link.href = canvas.toDataURL('image/jpeg',1);
-      link.setAttribute('download', donwload_name);
-      link.click();
-    });
+  html2canvas(elementToDownload).then(function(canvas) {
+    var link = document.createElement('a');
+    link.download = download_name;
+    link.href = canvas.toDataURL('image/jpeg',1);
+    link.setAttribute('download', download_name);
+    link.click();
+  });
 });
 
+// PNG Download
 document.getElementById('png').addEventListener('click', function () {
-  var donwload_name = `${file_name}_${p_value}_${log_ratio}_${abundance}_${algo}.png`;
-    var elementToDownload = document.getElementById('graph');
-    html2canvas(elementToDownload).then(function(canvas) {
-      var link = document.createElement('a');
-      link.download = 'mon-fichier.png';
-      link.href = canvas.toDataURL('image/png',10);
-      link.setAttribute('download', donwload_name);
-      link.click();
-    });
+  var download_name = `${file_name}_${p_value}_${log_ratio}_${abundance}_${algo}.png`;
+  var elementToDownload = document.getElementById('graph');
+  html2canvas(elementToDownload).then(function(canvas) {
+    var link = document.createElement('a');
+    link.download = download_name;
+    link.href = canvas.toDataURL('image/png',10);
+    link.setAttribute('download', download_name);
+    link.click();
+  });
 });
 
+// JSON Download
 document.getElementById('json').addEventListener('click', function () {
   var cy = window.cy;
-  var donwload_name = `${file_name}_${p_value}_${log_ratio}_${abundance}_${algo}.json`
+  var download_name = `${file_name}_${p_value}_${log_ratio}_${abundance}_${algo}.json`
 
   if (cy) {
     var cyjs = cy.json();
-    console.log(cyjs);
     var cyjs_string = JSON.stringify(cyjs);
     const a = document.createElement("a");
     const file = new Blob([cyjs_string], { type: "text/plain" });
     a.href = URL.createObjectURL(file);
-    a.download = donwload_name;
+    a.download = download_name;
     a.click();
   }
-
-  
 });
